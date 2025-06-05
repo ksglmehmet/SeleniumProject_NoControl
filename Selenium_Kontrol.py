876f0d4c8976bb00be054bceac3aec8423a6c538
ksglmehmet <mak.kosoglu@gmail.com>
2025-06-05 09:31:30 +0300

    Add initial project files including .gitignore, requirements, and Selenium script

---
+++ Selenium_Kontrol.py
@@ -0,0 +1,119 @@
+# Gerekli Kütüphanelerin import edilmesi #
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys 
+# Klavye komutlarını verebilmek için.
+from selenium.webdriver.support.ui import WebDriverWait 
+#time.sleep yerine, belirli bir elemanın görünür hale gelmesiyle işlem yapmasını sağlamak için.
+from selenium.webdriver.support import expected_conditions as EC 
+#time.sleep yerine, belirli bir elemanın görünür hale gelmesiyle işlem yapmasını sağlamak için.
+import time
+import path as ph
+import pandas as pd
+import os
+import numpy as np
+from selenium.common.exceptions import ElementClickInterceptedException
+
+# path = ph.path_driver2
+options = webdriver.FirefoxOptions()
+# options.add_experimental_option("detach", True)
+options.add_argument('--disable-notifications')
+options.add_argument("--disable-infobars")
+options.add_argument("start-maximized")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("--ignore-certificate-errors")
+
+driver = webdriver.Firefox(service = FirefoxService(GeckoDriverManager().install()))
+driver.maximize_window()
+
+driver.get(ph.url_proje_firefox)
+wait = WebDriverWait(driver, 20)
+
+UserName = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="otds_username"]')))
+UserName.click()
+UserName.send_keys(ph.User_Name)
+
+time.sleep(1)
+
+PassWord = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="otds_password"]')))
+PassWord.click()
+PassWord.send_keys(ph.User_Password)
+
+time.sleep(1)
+
+Sign = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="loginbutton"]')))
+Sign.click()
+
+time.sleep(2)
+Abone_Proje_Arsivi = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="rowCell0"]/td[3]/a[1]')))
+Abone_Proje_Arsivi.click()
+
+time.sleep(1)
+Proje_Arsivi = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="rowCell5"]/td[3]/a[1]')))
+Proje_Arsivi.click()
+
+time.sleep(1)
+Name_Sirala = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="browseViewCoreTable"]/tbody/tr[1]/td[3]/a')))
+Name_Sirala.click()
+
+time.sleep(1)
+I00001_2023 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="rowCell1"]/td[3]/a[1]')))
+I00001_2023.click()
+
+time.sleep(1)
+Name_Sirala_1 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="browseViewCoreTable"]/tbody/tr[1]/td[3]/a')))
+Name_Sirala_1.click()
+
+ArsivNo = "I00001"
+ArsivGrupNo = "I00001-2023"
+ProjeNo_Dogru = []
+ProjeNo_Yanlis = []
+
+time.sleep(1)
+# Toplam kaç sayfa ve tiff var ?
+Total_items = int(driver.find_element(By.XPATH, '/html/body/table/tbody/tr[2]/td/table/tbody/tr[2]/td/table/tbody/tr/td/form[3]/div/div/div/table/tbody/tr/td[2]/div/div[2]/table/tbody/tr/td/table/tbody/tr[2]/td[1]').text.split(" ")[-2])
+Total_page = int(np.ceil(Total_items / 25))
+
+print(f"\n I00001-2023 Klasöründe Toplam: {Total_items} Adet .tiff Dosyası Var.")
+# i = 1
+# for i in range(1, (Total_page + 1)):
+#     print(f"\n{i}. sayfada yer alan tifflerin bilgileri kontrol ediliyor.\n")
+#     page_count = []
+#     page_info = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="browseViewCoreTable"]'))).text.split("\n")
+#     for y in page_info:
+#         if y.strip().endswith(".tiff"):
+#             page = y.strip().replace(".tiff", "")
+#             page_count.append(page)
+
+time.sleep(0.5) # Minik bekleme
+try:
+    Ilgili_Tiff = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, f'#rowCell0 > td.browseItemName > a:nth-child(2)')))  
+    Ilgili_Tiff.click()
+    Properties = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="menuItem_Properties"]')))
+    Properties.click()
+except Exception as e:
+    raise Exception(f"Proje No'nun Özelliklerine Gidilemedi !!! Hata: {str(e)}")
+
+time.sleep(1) # Bazen, tiff adını alamıyor. Eski isimde kalıyor.
+try:
+    get_tiff = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/table/tbody/tr[2]/td/table/tbody/tr[1]/td/div/div[6]/div[2]'))).text.split(".")[0]
+except Exception as e:
+    raise Exception(f"Proje Numarası Kopyalanamadı !!! Hata: {e}")
+
+
+#time.sleep(1)
+try:
+    Categories = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="LLInnerContainer"]/tbody/tr[2]/td/table/tbody/tr[1]/td/form/table/tbody/tr[1]/td/ul/div[2]/li[5]')))
+    Categories.click()
+except Exception as e:
+    raise Exception(f"{get_tiff} No'lu Projenin Kategori Ekranına Geçilemedi !!! Hata: {e}")
+
+a = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#_1_1_2_1'))).get_attribute("value")
+if a == ArsivNo:
+    print(f"{get_tiff} No'lu Projenin Arşiv Numarası: {a} ve Doğru !")
+
